979cc770621f28676134abe586558c5d7e15babb
David Lee <Dai180293@gmail.com>
2023-11-26 04:49:21 +0000

    all styling has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,80 @@
+// import { useState, useEffect } from "react";
+// import differenceInYears from "date-fns/differenceInYears";
+// import differenceInMonths from "date-fns/differenceInMonths";
 function App() {
-    return <>hello</>;
+
+    return (
+        <section id="calculatorContainer">
+            <form>
+                <fieldset className="fieldSetContainer">
+                    <fieldset>
+                        <label htmlFor="day">day</label>
+                        <input
+                            type="text"
+                            id="day"
+                            placeholder="DD"
+                            // onChange={}
+                        />
+                        <span id="errorMessage">
+                            error message here
+                        </span>
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="month">month</label>
+                        <input
+                            type="text"
+                            id="month"
+                            placeholder="MM"
+                            // onChange={}
+                        />
+                        <span id="errorMessage">
+                            error message here
+                        </span>
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="year">year</label>
+                        <input
+                            type="text"
+                            id="year"
+                            placeholder="YYYY"
+                            // onChange={}
+                        />
+                        <span id="errorMessage">
+                            error message here
+                        </span>
+                    </fieldset>
+                </fieldset>
+                <div id="positionContainer">
+                    <div id="line"></div>
+                    <button id="submitButton">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="46"
+                            height="44"
+                            viewBox="0 0 46 44"
+                        >
+                            <g fill="none" stroke="#FFF" strokeWidth="2">
+                                <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+                            </g>
+                        </svg>
+                    </button>
+                </div>
+            </form>
+            <section className="ageContainer">
+                <ul>
+                    <li id="years">
+                        <span>--</span> years
+                    </li>
+                    <li id="months">
+                        <span>--</span> months
+                    </li>
+                    <li id="days">
+                        <span>--</span> days
+                    </li>
+                </ul>
+            </section>
+        </section>
+    );
 }
 
 export default App;
